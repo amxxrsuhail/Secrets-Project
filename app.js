37937a880dfcd5fa6f8acbdc2555088724610443
ameer suhail <kameer41099@gmail.com>
2022-12-07 18:00:05 +0530

    added basic boilerplate in app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const { log } = require("console");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(3000, () => {
+  console.log("server started at 3000");
+});
